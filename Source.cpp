41bf4b9c195860c623cb2730b404859154057ac0
franciscolinan <81978144+franciscolinan@users.noreply.github.com>
2021-09-01 23:19:13 -0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@
 #include <string>
 
 #if !defined(_WIN32) && !defined(_WIN64) 
-static_assert(false, "Este código está implementado para usarse en windows. Para usarlo en otro S.O. modifique las funciones GetAsyncKeyState y system");
+static_assert(false, "Este cÃ³digo estÃ¡ implementado para usarse en windows. Para usarlo en otro S.O. modifique las funciones GetAsyncKeyState y system");
 #endif
 
 struct Position
@@ -15,7 +15,7 @@ struct Position
 
     Position(const int x, const int y) : x(x), y(y) {}
 
-    inline bool operator == (const Position& position) const
+    inline auto operator == (const Position& position) const -> bool
     {
         // Operador para comparar 2 posiciones
         //
@@ -30,7 +30,7 @@ private:
     char board_[rows][columns] = { 0 }; // Tablero
 
 public:
-    void Print() const
+    auto Print() const -> void
     {
         // Imprimir tablero
         //
@@ -50,14 +50,14 @@ public:
         }
     }
 
-    inline void Clear() const
+    inline auto Clear() const -> void
     {
         // Limpiar pantalla
         //
         system("cls");
     }
 
-    inline char* operator [] (const std::size_t row)
+    inline auto operator [] (const std::size_t row) -> char*
     {
         // Operador para obtener el tablero
         //
@@ -65,14 +65,14 @@ public:
     }
 };
 
-// Dirección de la serpiente
+// DirecciÃ³n de la serpiente
 //
 const enum class Direction { kNone, kLeft, kRight, kUp, kDown };
 
 template<std::size_t rows, std::size_t columns>
 class Snake
 {
-    // Verificar tamaño del tablero
+    // Verificar tamaÃ±o del tablero
     //
     static_assert(rows >= 5, "La cantidad de filas tiene que ser mayor o igual a 5");
     static_assert(columns >= 5, "La cantidad de columnas tiene que ser mayor o igual a 5");
@@ -80,16 +80,16 @@ class Snake
 private:
     Board<rows, columns> board_;                            // Tablero
     std::vector<Position> snake_    = { Position(std::size_t(columns / 2), std::size_t(rows / 2)) };   // Serpiente
-    Direction current_direction_    = Direction::kNone;     // Dirección actual de la serpiente
+    Direction current_direction_    = Direction::kNone;     // DirecciÃ³n actual de la serpiente
     Direction keys_[2]              = { Direction::kNone }; // Arreglo de teclas presionadas
     Position target_                = Position(0, 0);       // Punto objetivo
     std::size_t difficulty_         = 0;                    // Dificultad de la serpiente
     clock_t time_                   = 0;                    // Contador tiempo
     std::size_t record_             = 0;                    // Record de puntaje
 
-    inline void MoveRight()
+    inline auto MoveRight() -> void
     {
-        // Verifica movimiento válido
+        // Verifica movimiento vÃ¡lido
         //
         if (this->current_direction_ != Direction::kLeft && this->current_direction_ != Direction::kRight && this->keys_[0] == Direction::kNone)
         {
@@ -104,9 +104,9 @@ private:
         }
     }
 
-    inline void MoveLeft()
+    inline auto MoveLeft() -> void
     {
-        // Verifica movimiento válido
+        // Verifica movimiento vÃ¡lido
         //
         if (this->current_direction_ != Direction::kRight && this->current_direction_ != Direction::kLeft && this->keys_[0] == Direction::kNone)
         {
@@ -121,9 +121,9 @@ private:
         }
     }
 
-    inline void MoveUp()
+    inline auto MoveUp() -> void
     {
-        // Verifica movimiento válido
+        // Verifica movimiento vÃ¡lido
         //
         if (this->current_direction_ != Direction::kDown && this->current_direction_ != Direction::kUp && this->keys_[0] == Direction::kNone)
         {
@@ -138,9 +138,9 @@ private:
         }
     }
 
-    inline void MoveDown()
+    inline auto MoveDown() -> void
     {
-        // Verifica movimiento válido
+        // Verifica movimiento vÃ¡lido
         //
         if (this->current_direction_ != Direction::kUp && this->current_direction_ != Direction::kDown && this->keys_[0] == Direction::kNone)
         {
@@ -155,9 +155,9 @@ private:
         }
     }
 
-    void UpdateBoard()
+    auto UpdateBoard() -> void
     {
-        // Actualiza la posición de la serpiente y el objetivo en el tablero
+        // Actualiza la posiciÃ³n de la serpiente y el objetivo en el tablero
         //
         for (std::size_t row = 0; row < rows; row++)
         {
@@ -184,9 +184,9 @@ private:
         }
     }
 
-    bool Update()
+    auto Update() -> bool
     {
-        // Actualiza la posición de la serpiente y verifica colisiones
+        // Actualiza la posiciÃ³n de la serpiente y verifica colisiones
         //
         Position next_position = this->snake_[0];
         bool is_valid_position = false;
@@ -284,7 +284,7 @@ public:
         this->UpdateBoard();
     }
 
-    bool Play()
+    auto Play() -> bool
     {
         // Verificar si el usuario presiona teclas de movimiento
         //
@@ -308,7 +308,7 @@ public:
             this->MoveDown();
         }
 
-        // Actualizar la posición de la serpiente cada 'difficulty_' tiempo
+        // Actualizar la posiciÃ³n de la serpiente cada 'difficulty_' tiempo
         //
         if (clock() - this->time_ > this->difficulty_)
         {
@@ -316,7 +316,7 @@ public:
             bool result = this->Update(); // Actualizar antes de imprimir para evitar lag
             this->board_.Clear(); // Limpiar pantalla
             this->board_.Print(); // Imprimir tablero
-            std::cout << "\n" << "Puntaje: " << this->snake_.size() - 1 << "\n"; // Imprimir puntuación
+            std::cout << "\n" << "Puntaje: " << this->snake_.size() - 1 << "\n"; // Imprimir puntuaciÃ³n
             std::cout << "Record: " << this->record_ << "\n";
 
             if (this->snake_.size() > this->record_)
@@ -334,11 +334,11 @@ public:
     }
 };
 
-int main()
+auto main() -> int
 {
     unsigned int difficulty = 0;
 
-    // Menú
+    // MenÃº
     //
     while (difficulty != 1 && difficulty != 2 && difficulty != 3 && difficulty != 4)
     {
